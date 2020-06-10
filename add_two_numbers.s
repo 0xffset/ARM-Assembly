fc8e7a4e15092b0082204be6368007ab68cc0da4
Rogger G. Diaz <roleyder>
2020-06-10 17:11:08 -0400

    Add two numbers

---
+++ add_two_numbers.s
@@ -0,0 +1,18 @@
+@
+@ This program add two numbers
+@
+
+	.text
+main: 	mov r0, #2      @ r0 <-- 1
+	mov r1, #3	@ r1 <-- 3
+	add r2, r0, r1  @ r2 <-- r0 + r1
+	ldr r2, = string
+	
+stop:	wfi
+.data
+string: 
+	.asciz "%d\n"
+
+ 
+
+	
