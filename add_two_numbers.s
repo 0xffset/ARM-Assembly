8196999d4bcf51d9b650a2e21dc3b44737797b75
roleyder <roleyder02@gmail.com>
2020-06-11 11:33:03 -0400

    fix

--- add_two_numbers.s
+++ add_two_numbers.s
@@ -5,11 +5,11 @@
 	.text
 main: 	mov r0, #2      @ r0 <-- 1
 	mov r1, #3	@ r1 <-- 3
-	add r2, r0 + r1  @ r2 <-- r0 + r1
+	add r2, r0, r1  @ r2 <-- r0 + r1
 	ldr r2, = string
 	
 stop:	wfi
-.data
+.data	
 string: 
 	.asciz "%d\n"
 
